5fc2f59b3a8a841adac4f582fc055232c6fa97b5
twistseason <pattanachai.twist@hotmail.com>
2021-10-15 23:14:09 +0700

    phases

--- component/planphases.js
+++ component/planphases.js
@@ -14,22 +14,27 @@ export default function planphases() {
         "
         style={{ color: "rgb(255,132,164)", height: "15vh" }}
       >
-        <h1 className=" uppercase  font-bold 
+        <h1
+          className=" uppercase  font-bold 
                     text-5xl    
                     2xl:text-7xl
                     xl:text-6xl 
                     lg:text-5xl 
                     md:text-5xl 
                     sm:text-5xl 
-        ">Phase</h1>
+        "
+        >
+          Phase
+        </h1>
       </div>
-      <div className=" pt-4 w-full flex h-4/5">
-        <div className="relative w-full ">
-          <img
-            className=" absolute top-0 left-0 object-cover object-top"
-            src="/images/12.jpg"
-            style={{ height: "185vh" }}
-          ></img>
+      <div className=" pt-4 w-full flex h-4/5 justify-center items-center">
+        <div
+          className="p-8 rounded-2xl"
+          style={{
+            backgroundColor: "rgba(0,0,0,0.6)",
+          }}
+        >
+          <img className=" " src="/images/12.png"></img>
         </div>
       </div>
     </>
