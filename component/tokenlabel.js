bcb9bbd25166779512c880f7503d805aacee9388
twistseason <pattanachai.twist@hotmail.com>
2021-10-29 18:25:37 +0700

    fix page

--- component/tokenlabel.js
+++ component/tokenlabel.js
@@ -9,12 +9,11 @@ export default function tokenlabel() {
       opacity: 1,
       xPercent: 0,
     });
-    
+
     gsap.to("#default", {
       opacity: 0,
       xPercent: -10,
     });
-  
   };
 
   const onLeave = ({ currentTarget }) => {
@@ -23,161 +22,351 @@ export default function tokenlabel() {
       xPercent: -10,
     });
   };
-  
+
   return (
     <>
+      {/* reward_pool */}
       <div
         id="#reward_pool"
-        className="my-3 flex items-center cursor-pointer"
+        className="my-0 flex cursor-pointer flex-col"
         onMouseEnter={onEnter}
         onMouseLeave={onLeave}
       >
-        <div
-          className=" w-8 h-4 border-2 border-white   mr-2"
-          style={{ backgroundColor: "#f9d801" }}
-        ></div>
-        <p className='
+        <div className="flex  items-center">
+          <div
+            className=" w-8 h-4 border-2 border-white mr-2 "
+            style={{ backgroundColor: "#f9d801" }}
+          ></div>
+          <p
+            className="
             text-base
             2xl:text-4xl
             xl:text-lg
             lg:text-lg
             md:text-lg
             sm:text-base
-        '>Reward pool</p>
+        "
+          >
+            Reward pool
+          </p>
+        </div>
+
+        <div className="flex items-center">
+          <div className=" w-8 h-4  mr-2 "></div>
+          <p
+            className="
+            text-base
+            2xl:text-4xl
+            xl:text-lg
+            lg:text-lg
+            md:text-lg
+            sm:text-base
+        "
+          >
+            20%
+          </p>
+        </div>
       </div>
 
+      {/* Play_to_Earn */}
       <div
         id="#Play_to_Earn"
-        className="my-3 flex items-center cursor-pointer"
+        className="my-0 flex flex-col cursor-pointer"
         onMouseEnter={onEnter}
         onMouseLeave={onLeave}
       >
-        <div
-          className=" w-8 h-4 border-2 border-white   mr-2"
-          style={{ backgroundColor: "#f69125" }}
-        ></div>
-        <p  className='
+        <div className="flex  items-center">
+          <div
+            className=" w-8 h-4 border-2 border-white mr-2 "
+            style={{ backgroundColor: "#f69125" }}
+          ></div>
+          <p
+            className="
             text-base
             2xl:text-4xl
             xl:text-lg
             lg:text-lg
             md:text-lg
             sm:text-base
-        '>Play to Earn</p>
+        "
+          >
+            Play to Earn
+          </p>
+        </div>
+
+        <div className="flex items-center">
+          <div className=" w-8 h-4  mr-2 "></div>
+          <p
+            className="
+            text-base
+            2xl:text-4xl
+            xl:text-lg
+            lg:text-lg
+            md:text-lg
+            sm:text-base
+        "
+          >
+            30%
+          </p>
+        </div>
       </div>
+
+      {/* Public_Sale */}
       <div
         id="#Public_Sale"
-        className="my-3 flex items-center cursor-pointer"
+        className="my-0 flex flex-col cursor-pointer"
         onMouseEnter={onEnter}
         onMouseLeave={onLeave}
       >
-        <div
-          className=" w-8 h-4 border-2 border-white   mr-2"
-          style={{ backgroundColor: "#f05f96" }}
-        ></div>
-        <p  className='
+        <div className="flex  items-center">
+          <div
+            className=" w-8 h-4 border-2 border-white mr-2 "
+            style={{ backgroundColor: "#f05f96" }}
+          ></div>
+          <p
+            className="
             text-base
             2xl:text-4xl
             xl:text-lg
             lg:text-lg
             md:text-lg
             sm:text-base
-        '>Public Sale</p>
+        "
+          >
+            Public Sale
+          </p>
+        </div>
+
+        <div className="flex items-center">
+          <div className=" w-8 h-4  mr-2 "></div>
+          <p
+            className="
+            text-base
+            2xl:text-4xl
+            xl:text-lg
+            lg:text-lg
+            md:text-lg
+            sm:text-base
+        "
+          >
+            9%
+          </p>
+        </div>
       </div>
+
+      {/* Private_Sale */}
       <div
         id="#Private_Sale"
-        className="my-3 flex items-center cursor-pointer"
+        className="my-0 flex flex-col cursor-pointer"
         onMouseEnter={onEnter}
         onMouseLeave={onLeave}
       >
-        <div
-          className=" w-8 h-4 border-2 border-white   mr-2"
-          style={{ backgroundColor: "#745ea7" }}
-        ></div>
-        <p  className='
+        <div className="flex  items-center">
+          <div
+            className=" w-8 h-4 border-2 border-white mr-2 "
+            style={{ backgroundColor: "#745ea7" }}
+          ></div>
+          <p
+            className="
             text-base
             2xl:text-4xl
             xl:text-lg
             lg:text-lg
             md:text-lg
             sm:text-base
-        '>Private Sale</p>
+        "
+          >
+            Private Sale
+          </p>
+        </div>
+
+        <div className="flex items-center">
+          <div className=" w-8 h-4  mr-2 "></div>
+          <p
+            className="
+            text-base
+            2xl:text-4xl
+            xl:text-lg
+            lg:text-lg
+            md:text-lg
+            sm:text-base
+        "
+          >
+            3%
+          </p>
+        </div>
       </div>
+
+      {/* Copyright_Holder */}
       <div
         id="#Copyright_Holder"
-        className="my-3 flex items-center cursor-pointer"
+        className="my-0 flex flex-col cursor-pointer"
         onMouseEnter={onEnter}
         onMouseLeave={onLeave}
       >
-        <div
-          className=" w-8 h-4 border-2 border-white   mr-2"
-          style={{ backgroundColor: "#0481c4" }}
-        ></div>
-        <p  className='
+        <div className="flex  items-center">
+          <div
+            className=" w-8 h-4 border-2 border-white mr-2 "
+            style={{ backgroundColor: "#0481c4" }}
+          ></div>
+          <p
+            className="
             text-base
             2xl:text-4xl
             xl:text-lg
             lg:text-lg
             md:text-lg
             sm:text-base
-        '>Copyright Holder</p>
+        "
+          >
+            Copyright Holder
+          </p>
+        </div>
+
+        <div className="flex items-center">
+          <div className=" w-8 h-4  mr-2 "></div>
+          <p
+            className="
+            text-base
+            2xl:text-4xl
+            xl:text-lg
+            lg:text-lg
+            md:text-lg
+            sm:text-base
+        "
+          >
+            7%
+          </p>
+        </div>
       </div>
+
+      {/* Party_team */}
       <div
         id="#Party_team"
-        className="my-3 flex items-center cursor-pointer"
+        className="my-0 flex flex-col cursor-pointer"
         onMouseEnter={onEnter}
         onMouseLeave={onLeave}
       >
-        <div
-          className=" w-8 h-4 border-2 border-white   mr-2"
-          style={{ backgroundColor: "#2aa8e2" }}
-        ></div>
-        <p  className='
+        <div className="flex  items-center">
+          <div
+            className=" w-8 h-4 border-2 border-white mr-2 "
+            style={{ backgroundColor: "#2aa8e2" }}
+          ></div>
+          <p
+            className="
             text-base
             2xl:text-4xl
             xl:text-lg
             lg:text-lg
             md:text-lg
             sm:text-base
-        '>3rd Party team</p>
+        "
+          >
+            3rd Party team
+          </p>
+        </div>
+
+        <div className="flex items-center">
+          <div className=" w-8 h-4  mr-2 "></div>
+          <p
+            className="
+            text-base
+            2xl:text-4xl
+            xl:text-lg
+            lg:text-lg
+            md:text-lg
+            sm:text-base
+        "
+          >
+            10%
+          </p>
+        </div>
       </div>
+
+      {/* Core_team */}
       <div
         id="#Core_team"
-        className="my-3 flex items-center cursor-pointer"
+        className="my-0 flex flex-col cursor-pointer"
         onMouseEnter={onEnter}
         onMouseLeave={onLeave}
       >
-        <div
-          className=" w-8 h-4 border-2 border-white   mr-2"
-          style={{ backgroundColor: "#75c5ac" }}
-        ></div>
-        <p  className='
+        <div className="flex  items-center">
+          <div
+            className=" w-8 h-4 border-2 border-white mr-2 "
+            style={{ backgroundColor: "#75c5ac" }}
+          ></div>
+          <p
+            className="
             text-base
             2xl:text-4xl
             xl:text-lg
             lg:text-lg
             md:text-lg
             sm:text-base
-        '>Core team</p>
+        "
+          >
+            Core team
+          </p>
+        </div>
+
+        <div className="flex items-center">
+          <div className=" w-8 h-4  mr-2 "></div>
+          <p
+            className="
+            text-base
+            2xl:text-4xl
+            xl:text-lg
+            lg:text-lg
+            md:text-lg
+            sm:text-base
+        "
+          >
+            13%
+          </p>
+        </div>
       </div>
+
+      {/* Ecosystem_Fund */}
       <div
         id="#Ecosystem_Fund"
-        className="my-3 flex items-center cursor-pointer"
+        className="my-0 flex flex-col cursor-pointer"
         onMouseEnter={onEnter}
         onMouseLeave={onLeave}
       >
-        <div
-          className=" w-8 h-4 border-2 border-white   mr-2"
-          style={{ backgroundColor: "#cee52e" }}
-        ></div>
-        <p  className='
+        <div className="flex  items-center">
+          <div
+            className=" w-8 h-4 border-2 border-white mr-2 "
+            style={{ backgroundColor: "#cee52e" }}
+          ></div>
+          <p
+            className="
             text-base
             2xl:text-4xl
             xl:text-lg
             lg:text-lg
             md:text-lg
             sm:text-base
-        '>Ecosystem Fund</p>
+        "
+          >
+            Ecosystem Fund
+          </p>
+        </div>
+
+        <div className="flex items-center">
+          <div className=" w-8 h-4  mr-2 "></div>
+          <p
+            className="
+            text-base
+            2xl:text-4xl
+            xl:text-lg
+            lg:text-lg
+            md:text-lg
+            sm:text-base
+        "
+          >
+            8%
+          </p>
+        </div>
       </div>
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import Chart from "../component/chart_pie";
 import Token_table from "../component/token_table";
 import Tokenlabel from "../component/tokenlabel";
 import Tokendetail from "../component/tokendetail";
-import Game from "../component/game";
+// import Game from "../component/game";
 import Phases from "../component/phases";
 
 export default function index() {
@@ -19,7 +19,7 @@ export default function index() {
       scroller: ".scroll",
     });
   }, []);
-  
+
   const godownEnter = ({ currentTarget }) => {
     // let tokendetail = currentTarget.target.value
     console.log(currentTarget);
@@ -37,7 +37,7 @@ export default function index() {
   };
   return (
     <>
-      <style > 
+      <style>
         {`
    
     .scroll {
@@ -61,18 +61,20 @@ export default function index() {
         }}
       >
         <section className="panel header">
-          <Header></Header> 
+          <Header></Header>
         </section>
 
         <section className="panel vision">
           <Vision></Vision>
         </section>
 
-        <section   className="panel role bg-cover bg-bottom bg-no-repeat bg-scroll"  
-        style={{
-          backgroundImage: "url('/images/Role_Bg.png')",
-        }}>     
-        <div
+        <section
+          className="panel role bg-cover bg-bottom bg-no-repeat bg-scroll"
+          style={{
+            backgroundImage: "url('/images/Role_Bg.png')",
+          }}
+        >
+          <div
             className="w-full mb-2 text-center flex justify-center items-center
                     px-4
                     2xl:px-12
@@ -96,10 +98,9 @@ export default function index() {
             >
               role in rhyquiem
             </h1>
-           
           </div>
-          <Role ></Role>          
-        </section>     
+          <Role></Role>
+        </section>
 
         <section
           id="tokennomic"
@@ -109,14 +110,33 @@ export default function index() {
             className="pt-4 w-full text-center"
             style={{ color: "rgb(255,132,164)" }}
           >
-            <h1 className=" uppercase font-bold
+            <h1
+              className=" uppercase font-bold mb-4
                         text-3xl
                         2xl:text-7xl
                         xl:text-5xl
                         lg:text-6xl
                         md:text-5xl
                         sm:text-5xl
-            ">tokennomics</h1>
+            "
+            >
+              tokennomics
+            </h1>
+          
+          </div>
+          <div
+            className=" w-full text-center"
+            style={{ color: "rgb(255,132,164)" }}
+          >
+            <h1  className=" uppercase font-bold text-white
+                        text-lg
+                        2xl:text-3xl
+                        xl:text-2xl
+                        lg:text-xl
+                        md:text-lg
+                        sm:text-lg
+            ">100,000,000 Token</h1>
+          
           </div>
           <div
             className="w-full   justify-center items-center
@@ -127,12 +147,13 @@ export default function index() {
                           md:flex       md:py-12
                           sm:flex       sm:py-10
             
-            " >
+            "
+          >
             <Token_table></Token_table>
           </div>
 
           <div
-             className="w-full px-14  justify-center items-center
+            className="w-full px-14  justify-center items-center
              hidden
              2xl:flex
              xl:flex
@@ -144,14 +165,16 @@ export default function index() {
             <div className="w-4/12">
               <Chart></Chart>
             </div>
-            <div className="w-4/12 text-white text-xl 
+            <div
+              className="w-2/12 text-white text-xl 
                   pl-2
                   2xl:pl-6
                   xl:pl-4
                   lg:pl-4
                   md:pl-2
                   sm:pl-2
-            ">
+            "
+            >
               <Tokenlabel></Tokenlabel>
             </div>
             <div className="w-6/12 py-8 px-12 h-4/5">
@@ -162,9 +185,11 @@ export default function index() {
           </div>
         </section>
 
-        <section className="panel Game">
+        {/* <section className="panel Game">
           <Game></Game>
-        </section>
+        </section> */}
+
+
         <section className="panel relative phases  flex flex-col justify-end items-center">
           <div
             className="w-full mb-2 text-right flex justify-end items-end
@@ -190,10 +215,9 @@ export default function index() {
             >
               phases
             </h1>
-           
           </div>
           <Phases></Phases>
-          <div className="absolute bottom-4 w-full flex flex-col justify-center items-center">
+          {/* <div className="absolute bottom-4 w-full flex flex-col justify-center items-center">
             <div
               className="cursor-pointer "
               onMouseEnter={godownEnter}
@@ -227,11 +251,8 @@ export default function index() {
                 />
               </a>
             </div>
-          </div>
+          </div> */}
         </section>
-
-
-
       </div>
 
       {/* <Role></Role> */}
