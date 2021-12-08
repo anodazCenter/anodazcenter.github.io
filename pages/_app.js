e466dff1f99a4c2707f4fed56e2d1f3a8a15baa0
twistseason <pattanachai.twist@hotmail.com>
2021-12-08 13:34:00 +0700

    head

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,18 @@
-import '../styles/globals.css'
-import '../styles/phaseline.css'
-import '../styles/modelvision.css'
-
+import "../styles/globals.css";
+import "../styles/phaseline.css";
+import "../styles/modelvision.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
+    <>
+      <Head>
+        <title>Streami-Fi</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Component {...pageProps} />
-    )
-    
+    </>
+  );
 }
 
-export default MyApp
-
+export default MyApp;
