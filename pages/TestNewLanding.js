cb9baae448453cc7cfec99b0db29de9779fb527d
twistseason <39827654+twistseason@users.noreply.github.com>
2021-12-15 16:39:57 +0700

    gg

--- pages/TestNewLanding.js
+++ pages/TestNewLanding.js
@@ -1,33 +1,19 @@
-import React from 'react'
+import React from "react";
 
 export default function TestNewLanding() {
-    return (
-        <div className='w-full text-center'> 
-            <h1>
-                hello streami-fi GG
-            </h1>
-            <h2>
-                hello streami-fi GG
-            </h2>
-            <h3>
-                hello streami-fi GG
-            </h3>
-            <h4>
-                hello streami-fi GG
-            </h4>
-            <h5>
-                hello streami-fi GG
-            </h5>
-            <h6>
-                hello streami-fi GG
-            </h6>
-            <br/>
-            <br/>
-            <p className='title'>
-                hello streami-fi GG
-            </p>
-            {/* form input */}
-            
-        </div>
-    )
+  return (
+    <div className="w-full text-center">
+      <h1>hello streami-fi GG</h1>
+      <h2>hello streami-fi GG</h2>
+      <h3>hello streami-fi GG</h3>
+      <h4>hello streami-fi GG</h4>
+      <h5>hello streami-fi GG</h5>
+      <h6>hello streami-fi GG</h6>
+      <br />
+      <br />
+      <p className="title">hello streami-fi GG</p>
+      <p className="sub-title">hello streami-fi GG</p>
+      {/* form input */}
+    </div>
+  );
 }
