f353a88922903264dceeb7aefd50468b1a1c87ad
twistseason <39827654+twistseason@users.noreply.github.com>
2021-12-16 19:01:30 +0700

    Update header.js

--- component/header.js
+++ component/header.js
@@ -9,6 +9,9 @@ export default function header() {
           <div className="mifi-header-logo">
             <img src="/images/logo_Streami-fi.png" alt="logo" />
           </div>
+            <div className="mifi-header-bottom mt-auto flex p-16">
+
+            </div>
         </div>
       </div>
     </>
