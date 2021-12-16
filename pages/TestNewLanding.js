f00108482bacf4d38e31923d80f6df35c0503a74
twistseason <39827654+twistseason@users.noreply.github.com>
2021-12-16 16:59:07 +0700

    tailwind set

--- pages/TestNewLanding.js
+++ pages/TestNewLanding.js
@@ -2,25 +2,11 @@ import React from "react";
 
 export default function TestNewLanding() {
   return (
-    <div className="w-full text-center">
-      <h1>hello streami-fi GG</h1>
-      <h2>hello streami-fi GG</h2>
-      <h3>hello streami-fi GG</h3>
-      <h4>hello streami-fi GG</h4>
-      <h5>hello streami-fi GG</h5>
-      <h6>hello streami-fi dfgsfdgd</h6>
-      <br />
-      <br />
-      <p className="title">hello streami-fi GG</p>
-      <p className="sub-title">hello streami-fi GG</p>
-      <button>hello streami-fi GG</button>
-      <br/>
-      <br/>
-      <a className="bn39 mx-auto" href="#"><span className="bn39span">Button</span></a>
-      <button className="sf-btn">
-        <span className="sf-btn-text">Button</span>
-      </button>
-      {/* form input */}
-    </div>
+  <>
+  {/* header */}
+  <div className="header">
+
+  </div>
+  </>
   );
 }
