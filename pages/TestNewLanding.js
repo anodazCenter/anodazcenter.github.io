805e33432b1ff4c08b2bd7afdc338bd24cd487f2
twistseason <39827654+twistseason@users.noreply.github.com>
2021-12-18 01:11:58 +0700

    ...

--- pages/TestNewLanding.js
+++ pages/TestNewLanding.js
@@ -1,27 +1,10 @@
-import Header from "../component/header";
-import Vision from "../component/vision";
-import Case from "../component/case";
-import Asset from "../component/asset";
-import Token from "../component/token";
-import Roadmap from "../component/roadmap";
-import Footer from "../component/footer";
 import React from "react";
 
 export default function TestNewLanding() {
   return (
     <>
       {/* header */}
-      <div className="mifi-web-background">
-        <div className="mifi-layout-margin">
-          <Header />
-          <Vision/>
-          <Case/>
-          <Asset/>
-          <Token/>
-          <Roadmap/>
-        </div>
-          <Footer/>
-      </div>
+      
     </>
   );
 }
