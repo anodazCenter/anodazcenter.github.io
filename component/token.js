8fbea130f8252907717d48ddd5d738529e0e4c83
twistseason <39827654+twistseason@users.noreply.github.com>
2021-12-22 15:08:39 +0700

    new token

--- component/token.js
+++ component/token.js
@@ -4,17 +4,8 @@ export default function token() {
     return (
         <>
         <div className="mifi-token">
-            <div className="mifi-token-title">
-                <h3>Tokenomics</h3>
-            </div>
             <div className="mifi-token-content">
-                <img src='/images/token.svg ' alt=''/>
-            </div>
-        </div>
-        <div className="mifi-token-m">
-                <h3>Tokenomics</h3>
-            <div className="mifi-token-content-m">
-                <img src='/images/token.svg ' alt=''/>
+                <img src='/images/tokenmics.png ' alt=''/>
             </div>
         </div>
         </>
