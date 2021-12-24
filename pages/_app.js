8adf0c7ca5169c68f2f50c41cc2da147823b8d0a
twistseason <39827654+twistseason@users.noreply.github.com>
2021-12-24 14:54:35 +0700

    .

--- pages/_app.js
+++ pages/_app.js
@@ -19,7 +19,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
         <title>Streami-Fi</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
