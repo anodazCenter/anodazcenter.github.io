b552886faed01ed092a7f5fd3593cf88c9e43071
twistseason <39827654+twistseason@users.noreply.github.com>
2021-12-24 14:21:18 +0700

    .

--- component/case.js
+++ component/case.js
@@ -14,8 +14,8 @@ export default function Case() {
           <img className="mifi-case-node-m" src="/images/case_node_M.svg"></img>
           <div className="mifi-case-content-text-1">
             <h5 className="case-title ">Streami-fi</h5>
-            <h5 className="case-title ">Headphones</h5>
-            <p className="case-sub-title-1 sub-title">
+            <h5 className="case-title ">Stereo turntables</h5>
+            <p className="case-sub-title-1 param-3">
               For a limited time, a random headphones box will be available for
               purchase, and users who own headphones will be rewarded with more
               SMF tokens while playinggames or listening to music. The more SMF
@@ -26,7 +26,7 @@ export default function Case() {
 
           <div className="mifi-case-content-text-2">
             <h5 className="case-title ">Year of Purchase</h5>
-            <p className="case-sub-title-2 sub-title">
+            <p className="case-sub-title-2 param-3">
               Match the year of purchase of the headphones to the song for extra
               tokens!
             </p>
@@ -34,7 +34,7 @@ export default function Case() {
 
           <div className="mifi-case-content-text-3">
             <h5 className="case-title ">Year of Purchase</h5>
-            <p className="case-sub-title-3 sub-title">
+            <p className="case-sub-title-3 param-3">
               Match the year of purchase of the headphones to the song for extra
               tokens!
             </p>
@@ -42,14 +42,12 @@ export default function Case() {
 
           <div className="mifi-case-content-text-4">
             <h5 className="case-title ">Gift</h5>
-            <p className="case-sub-title-4 sub-title">
-            Users can purchase Gifts to send to someone 
-they like while they perform or play the game.
-The higher the value of the gift, the more tokens
-the recipient will receive.
+            <p className="case-sub-title-4 param-3">
+              Users can purchase Gifts to send to someone they like while they
+              perform or play the game. The higher the value of the gift, the
+              more tokens the recipient will receive.
             </p>
           </div>
-
         </div>
       </div>
     </>
